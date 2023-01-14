f1d88e25ddecd15ae54e222b46f3e4344c8b1e94
iamkredig <kornienkotati79@gmail.com>
2023-01-14 21:24:53 +0100

    happy bd

---
+++ script.js
@@ -0,0 +1,30 @@
+let ScreenWidth = window.innerWidth
+let ScreenLengh = window.innerHeight
+
+function Setpadding () {document.documentElement.style.setProperty('--padding', window.innerHeight / 100 * 7 + "px");}
+
+if (ScreenWidth >= 700) {
+    window.addEventListener("resize", e=> { Setpadding();
+    });
+    Setpadding();
+}
+
+PasswordInputField = document.querySelector("#password-input")
+body = document.querySelector("body")
+welcome = document.querySelector(".welcome")
+
+PasswordInputField.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter' && PasswordInputField.value.toUpperCase() === "GRAVITY") {
+        welcome.classList.add("active");
+        body.classList.remove("blocked");
+    }
+    if (e.key === 'Enter' && PasswordInputField.value.toUpperCase() !== "GRAVITY") {
+        welcome.classList.remove("active");
+        setTimeout(function(){
+            body.classList.add("blocked");
+
+        }, 1); 
+        body.classList.remove("blocked");
+    }
+      
+});
